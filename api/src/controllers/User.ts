a2feae61b9bee76d7c4610c1a5e46b6ca93e8b4c
JoaoVitorLima242 <jlima0500@gmail.com>
2022-09-28 22:54:33 -0300

    get user by token

--- api/src/controllers/User.ts
+++ api/src/controllers/User.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from 'express'
 
+// Helpers
+import Token from '../helpers/token'
+
 type UpdateUser = {
     email: string;
     name: string;
@@ -9,13 +12,19 @@ type UpdateUser = {
 
 class UserControllers {
   public async getUserByToken (req: Request, res: Response): Promise<Response> {
-    const token = req.header('auth-token')
+    try {
+      const token = req.header('auth-token')
 
-    if (!token) {
-      return res.status(401).json({ error: 'Acesso negado!' })
+      if (!token) {
+        return res.status(401).json({ error: true, message: 'Acesso negado!' })
+      }
+
+      const user = await Token.getUser(res, token)
+      
+      return res.json({ error: null, user })
+    } catch (error) {
+      res.status(400).json({ error: 'O usuário não foi encontrado!' })
     }
-    
-    return res.json({token})
   }
 
 
--- api/src/helpers/token.ts
+++ api/src/helpers/token.ts
@@ -1,27 +1,42 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from 'jsonwebtoken'
 
+// Models
+import UserSchema from '../models/User'
+import { UserInterface } from '../models/User/index.d'
+
+
 interface TokenRequest extends Request {
     user?: string | jwt.JwtPayload;
   }
 
 class Token {
-    public check = (req: TokenRequest, res: Response, next: NextFunction): Response => {
-        const token = req.header('auth-token')
-    
-        if (!token) {
-          return res.status(401).json({ error: 'Acesso negado!' })
-        }
-    
-        try {
-          const verified = jwt.verify(token, process.env.TOKEN_SECRET)
-          req.user = verified
-          next()
-        } catch (error) {
-          res.status(400).json({ error: 'O token é invalido!' })
-        }
-      }
-    
+  public check = (req: TokenRequest, res: Response, next: NextFunction): Response => {
+    const token = req.header('auth-token')
+
+    if (!token) {
+      return res.status(401).json({ error: 'Acesso negado!' })
+    }
+
+    try {
+      const verified = jwt.verify(token, process.env.TOKEN_SECRET)
+      req.user = verified
+      next()
+    } catch (error) {
+      res.status(400).json({ error: 'O token é invalido!' })
+    }
+  }
+
+  public getUser = async (res: Response, token: string): Promise<UserInterface | null> => {
+    const decoded = jwt.verify(token, process.env.TOKEN_SECRET)
+    if (!token) {
+      res.status(401).json({ error: 'Acesso negado!' })
+      return null
+    }
+    const user = await UserSchema.findOne({ _id: (<jwt.JwtPayload>decoded).id })
+
+    return user
+  }
 }
 
 export default new Token()
