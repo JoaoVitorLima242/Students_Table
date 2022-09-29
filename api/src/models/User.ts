2d183a36fae7b977873c50d4f5e00edf136fb2dd
JoaoVitorLima242 <jlima0500@gmail.com>
2022-09-28 21:18:38 -0300

    add user model

---
+++ api/src/models/User.ts
@@ -0,0 +1,13 @@
+import { Schema, model } from 'mongoose'
+import { UserInterface } from './index'
+
+const UserSchema = new Schema<UserInterface>({
+  name: { type: String },
+  email: { type: String, required: true },
+  password: { type: String, required: true }
+},
+{
+  timestamps: true
+})
+
+export default model<UserInterface>('User', UserSchema)
