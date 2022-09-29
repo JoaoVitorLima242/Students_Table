e81c4653fdd36118cdadd35ceacafd3e78df768f
JoaoVitorLima242 <jlima0500@gmail.com>
2022-09-28 21:30:02 -0300

    adjusting eslint bug

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
   extends: 'standard-with-typescript',
   overrides: [
   ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module'
