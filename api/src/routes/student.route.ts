d133f33f9e5b467ec674d850cf1092b4255e1754
JoaoVitorLima242 <jlima0500@gmail.com>
2022-10-02 19:08:17 -0300

    add token check

--- api/src/routes/student.route.ts
+++ api/src/routes/student.route.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express'
 import StudentControllers from '../controllers/Student'
+import token from '../helpers/token'
 
 const routes = Router()
 
-routes.post('/', StudentControllers.createStudent)
-routes.get('/', StudentControllers.getStudents)
-routes.get('/:id', StudentControllers.getStudentById)
-routes.put('/:id', StudentControllers.updateUser)
-routes.delete('/:id', StudentControllers.deleteStudent)
+routes.post('/',token.check, StudentControllers.createStudent)
+routes.get('/', token.check, StudentControllers.getStudents)
+routes.get('/:id', token.check, StudentControllers.getStudentById)
+routes.put('/:id', token.check, StudentControllers.updateUser)
+routes.delete('/:id', token.check, StudentControllers.deleteStudent)
 
 export default routes
